a8751b9c3f3b8ea98c949e3ffcf9fa921aa22231
Emanuel-js <57919304+Emanuel-js@users.noreply.github.com>
2021-09-15 20:01:47 +0300

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,17 @@
 #!/usr/bin/python3
-'''
-gathers information about an employee by ID and returns their TODO progress
-'''
+"""Script to export data in the CSV format"""
 import csv
-import os
-import requests
+import requests as r
 import sys
 
-
-def main(argv):
-
-    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                       .format(sys.argv[1]))
-    name = emp.json().get('username')
-    tasks = requests.get('https://jsonplaceholder.typicode.com/todos')
-    tasks = tasks.json()
-    complete = 0
-    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-    titles = []
-    total = 0
-    filename = sys.argv[1] + ".csv"
-    with open(filename, 'w') as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL, quotechar='"')
-        for task in tasks:
-            if task['userId'] == int(sys.argv[1]):
-                task['name'] = name
-                writer.writerow([task['userId'], task['name'],
-                                 task['completed'], task['title']])
-
-
 if __name__ == "__main__":
-    import sys
-    main(sys.argv)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    usr = r.get(url + "users/{}".format(user_id)).json()
+    username = usr.get("username")
+    to_do = r.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow([user_id, username, elm.get("completed"),
+                          elm.get("title")]) for elm in to_do]
