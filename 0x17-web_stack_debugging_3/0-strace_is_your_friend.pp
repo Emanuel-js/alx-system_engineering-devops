cb29e4fed2296ab8ee687a720ee596a2f0d97c30
Emanuel <57919304+Emanuel-js@users.noreply.github.com>
2021-11-18 12:00:11 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-	# fix class-wp-locale.phpp to class-wp-locale.php
-exec { 'change-extensionspelling':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+# Fix 500 error when a GET HTTP method is requested to Apache web server
+
+exec {'replace':
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
